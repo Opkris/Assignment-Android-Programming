3ac7961566fb4ac68afd219709b9d76360b7fb82
OpKris <kristofferopdahl@mail.com>
2020-04-14 21:50:03 +0200

    done some refactoring, and added SwipeLayout

--- app/src/main/java/no/kristiania/assignment_noforeignland/MainActivity.kt
+++ app/src/main/java/no/kristiania/assignment_noforeignland/MainActivity.kt
@@ -10,10 +10,15 @@ import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.room.Room
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 import no.kristiania.assignment_noforeignland.adapters.MainAdapter
 import no.kristiania.assignment_noforeignland.db.PlaceDB
 import no.kristiania.assignment_noforeignland.db.model.PlaceEntity
+import no.kristiania.assignment_noforeignland.models.Location
+import no.kristiania.assignment_noforeignland.splash.SplashActivity
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -33,14 +38,22 @@ class MainActivity : AppCompatActivity() {
 
         renderLocations(listOfLocations)
 
-        // Swipe Widget
+
+        var swipeCounter = 0
+        // Swipe Widget to update list of locations.
         swipeLayout = findViewById(R.id.swipeContainer)
         swipeLayout.setOnRefreshListener {
-            println("hello World Swipe")
             renderLocations(listOfLocations)
             swipeLayout.isRefreshing = false
+            swipeCounter += 1
+
+            if (swipeCounter >= 3){
+                Log.d(TAG,"New Fetch request to the API")
+                fetchJson(db)
+                swipeCounter = 0
+            }
         }
-        }// end onCreate
+    }// end onCreate
 
     private fun renderLocations(listOfLocations: List<PlaceEntity>) {
         Log.d(TAG, "getting all Locations")
@@ -52,7 +65,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-
         val inflater = menuInflater
         inflater.inflate(R.menu.main, menu)
 
@@ -73,6 +85,50 @@ class MainActivity : AppCompatActivity() {
         })
         return true
     }
+
+    fun fetchJson(db: PlaceDB) {
+        println("Attempting to Fetch JSON")
+
+        val url = "https://www.noforeignland.com/home/api/v1/places/"
+        val request = Request.Builder().url(url).build()
+        val client = OkHttpClient()
+
+        //enqueue make sure that every thing is going on in the background // another thread.
+        client.newCall(request).enqueue(object : Callback {
+
+            override fun onResponse(call: Call, response: Response) {
+                val body = response.body?.string()
+
+                // if you need to do debug the response.body :
+//                println("from Json Body: \n $body ")
+
+                val gson = GsonBuilder().create()
+                val locations = gson.fromJson(body, Location::class.java)
+
+                    Log.d("Database", "Storing data to local")
+
+                    //Looping through homeFeed.features list
+                    locations.features.forEach {
+
+                        val thread = Thread {
+                            //Setting values to Database
+                            val placeEntity = PlaceEntity()
+                            placeEntity.placeId = it.properties.id
+                            placeEntity.placeName = it.properties.name
+                            placeEntity.placeLon = it.geometry.coordinates[0]
+                            placeEntity.placeLat = it.geometry.coordinates[1]
+
+                            db.placeDao().savePlaces(placeEntity)
+                        }
+                        thread.start()
+                    }// end forloop
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Failed to execute request")
+            }
+        })// end client.newCall
+    }// end fetchJson
 }
 
 
